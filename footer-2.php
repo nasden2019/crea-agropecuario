8cc22e65a6550353b755f9be89c342788c74225c
Alex <alex@syloper.com>
2020-03-31 16:04:59 -0300

    proyectos a wordp

---
+++ footer-2.php
@@ -0,0 +1,100 @@
+<!-- FOOTER -->
+<footer id="clientes" class="mt-sm-5" style="height: 100%;">
+        <!-- <h2 class="text-center font-weight-bold pb-5 mt-sm-3">CLIENTES</h2> -->
+        <!-- owl carrousel -->
+        <div class="container">
+            <div class="owl-carousel owl-theme">
+                <div><img src="images/logo2.png" alt="" class="owl-images rounded-circle mx-auto"
+                        style="width: 80px;" />
+                </div>
+                <div><img src="images/logo2.png" alt="" class="owl-images rounded-circle mx-auto"
+                        style="width: 80px;" />
+                </div>
+                <div><img src="images/logo2.png" alt="" class="owl-images rounded-circle mx-auto"
+                        style="width: 80px;" />
+                </div>
+                <div><img src="images/logo2.png" alt="" class="owl-images rounded-circle mx-auto"
+                        style="width: 80px;" />
+                </div>
+                <div><img src="images/logo2.png" alt="" class="owl-images rounded-circle mx-auto"
+                        style="width: 80px;" />
+                </div>
+                <div><img src="images/logo2.png" alt="" class="owl-images rounded-circle mx-auto"
+                        style="width: 80px;" />
+                </div>
+                <div><img src="images/logo2.png" alt="" class="owl-images rounded-circle mx-auto"
+                        style="width: 80px;" />
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
+    <!-- loader -->
+    <!-- jQuery CDN - Slim version (=without AJAX) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <!-- Popper.JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"
+        integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous">
+    </script>
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"
+        integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous">
+    </script>
+
+    <script src="js/main.js"></script>
+
+    <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+            <circle mx-auto class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+            <circle mx-auto class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
+                stroke="#f4b214" /></svg></div>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/main.js"></script>
+
+
+    <script>
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggle();
+            });
+        });
+
+        $(".owl-carousel").owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: false,
+            autoplay: true,
+            autoplayTimeout: 2000,
+            responsive: {
+                // items: 6,
+                // itemsDesktop: [1000, 6],
+                // itemsDesktopSmall: [900, 3],
+                // itemsTablet: [600, 2],
+                // itemsMobile: [480, 1]
+                0: {
+                    dotsEach: 5,
+                    items: 1
+                },
+                600: {
+                    dotsEach: 4,
+                    items: 3
+                },
+                1200: {
+                    dotsEach: 3,
+                    items: 5
+                }
+            }
+        });
+        // SIDEBAR
+    </script>
+</body>
+</html>
---
+++ proyectos.php
@@ -0,0 +1,203 @@
+<?php
+/*
+Template Name: proyectos
+*/
+?>
+<?php get_header(); ?>
+
+<!-- SIDEBAR -->
+    <!-- METER LA PANTALLA ENTERA (antes de q termine el sidebar) DENTRO DEL SIDE BAR (MENOS EL NAVBAR Y EL FOOTER, PARA Q NO SE ROMPAN!)  -->
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar" style="display: none">
+            <div class="sidebar-header">
+                <h3>Testeos Generales</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <!-- <p>Dummy Heading</p> -->
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                        CATEGORIAS
+                    </a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="#">Categorias 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Categorias 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Categorias 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="text-white" href="#">SOBRE NOSOTROS</a>
+                </li>
+                <li>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle 
+                        text-white">HERRAMIENTAS</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a class="text-white" href="#">Herramientas 1</a>
+                        </li>
+                        <li>
+                            <a class="text-white" href="#">Herramientas 2</a>
+                        </li>
+                        <li>
+                            <a class="text-white" href="#">Herramientas 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="text-white" href="#">CONTACTO</a>
+                </li>
+            </ul>
+
+        </nav>
+
+        <!-- METER ACA!!!! LA PANTALLA ENTERA DENTRO DEL SIDE BAR (MENOS EL NAVBAR Y EL FOOTER, PARA Q NO SE ROMPAN!)  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-contacto">
+                        <i class="fas fa-align-left"></i>
+                        <span>Info extra</span>
+                    </button>
+                    <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <i class="fas fa-align-justify"></i>
+                    </button>
+                </div>
+            </nav>
+
+            <!-- END SIDEBAR -->
+
+
+            <div class="top-shadow"></div>
+
+            <section class="mt-sm-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-6">
+                            <h3 class="mainTitular">| Proyectos</h3>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section class="section">
+                <div class="container">
+                    <div class="row">
+
+
+                        <!-- aca iban los botones de la izquierda -->
+
+
+                        <div class="col-md-6 d-none d-md-block">
+                            <!-- text-center: para q ocupe el centro-->
+                            <div class="text-center">
+                                <iframe width="100%" height="405" src="https://www.youtube.com/embed/ijs_p00fkdU"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
+                        </div>
+
+                        <!-- video en mobile-->
+                        <div class="col-md-5 d-md-none mt-4">
+                            <div class="">
+                                <iframe width="100%" height="330" src="https://www.youtube.com/embed/ijs_p00fkdU"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
+                        </div>
+
+                        <!-- img -->
+                        <div class="col-md-6">
+                            <div class="">
+                                <img src="images/img_1.jpg" class="img-fluid">
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <div>
+                                <span class="mainTitular titularchiquito">| Categorias</span>
+                                <h2 class="d-md-block d-none">Nueva herramienta para implementar en la empresa lechera
+                                </h2>
+                                <p class="p-convocatorias">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                    Aliquam
+                                    ipsum
+                                    minima nesciunt magni
+                                    reprehenderit ullam sunt? Reiciendis, quisquam consectetur quae eveniet rem minima
+                                    ipsam
+                                    molestiae aperiam, modi eveniet quam dolorem officia non reprehenderit ut? Pariatur
+                                    dictaam,
+                                    incidunt quo adipisci officiis minus voluptatum veritatis?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- SEGUNDA SECCION -->
+            <section class="section mt-md-5">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-md-6 d-none d-md-block">
+                            <!-- text-center: para q ocupe el centro-->
+                            <div class="text-center">
+                                <iframe width="100%" height="405" src="https://www.youtube.com/embed/ijs_p00fkdU"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
+                        </div>
+
+                        <!-- video en mobile-->
+                        <div class="col-md-5 d-md-none mt-4">
+                            <div class="">
+                                <iframe width="100%" height="330" src="https://www.youtube.com/embed/ijs_p00fkdU"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                            </div>
+                        </div>
+
+                        <!-- img -->
+                        <div class="col-md-6">
+                            <div class="">
+                                <img src="images/img_1.jpg" class="img-fluid">
+                            </div>
+                        </div>
+
+                        <div class="col-md-12">
+                            <div>
+                                <span class="mainTitular titularchiquito">| Herramientas</span>
+                                <h2 class="d-md-block d-none">Nueva herramienta para implementar en la empresa lechera
+                                </h2>
+                                <p class="p-convocatorias">Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                    Aliquam
+                                    ipsum
+                                    minima nesciunt magni
+                                    reprehenderit ullam sunt? Reiciendis, quisquam consectetur quae eveniet rem minima
+                                    ipsam
+                                    molestiae aperiam, modi eveniet quam dolorem officia non reprehenderit ut? Pariatur
+                                    dictaam,
+                                    incidunt quo adipisci officiis minus voluptatum veritatis?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+
+    
+    <?php get_footer('2'); ?>
