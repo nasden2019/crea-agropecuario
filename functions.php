14332d8fabcf135978eb927e95079750ea8b806c
Gino Caputo <ginopq@gmail.com>
2020-04-08 13:40:49 -0300

    cambios menu testeo

--- functions.php
+++ functions.php
@@ -91,6 +91,15 @@ if ( function_exists( 'add_theme_support' ) ) {
         'dashicons-admin-home',
         40
     );
+    add_menu_page(
+        'testeos',
+        'testeos',
+        'read',
+        'testeos',
+        '',
+        'dashicons-admin-home',
+        40
+    );
 
  }
 
