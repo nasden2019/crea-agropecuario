4acad4209550630bd65df4710717146ce5c6025c
Gino Caputo <ginopq@gmail.com>
2020-04-14 11:46:41 -0300

    cambios en publicaciones

--- trabajos.php
+++ trabajos.php
@@ -24,109 +24,110 @@ Template Name: trabajos
             <div class="row">
                 <div class="col-sm-8 mt-md-3">
                     <div class="row">
-                        <div class="col-sm-8 mt-md-3">
-                            <div class="row">
-                    <?php $args = array('post_type' => 'trabajos_home'); ?>
-                     <?php $loop = new WP_Query($args);
-                       $i=1;
-                        if ( $loop->have_posts() ) :
-                          while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                        <?php $args = array('post_type' => 'trabajos_home'); ?>
+                        <?php $loop = new WP_Query($args); ?>
+                        <? $i=1;
+                            if ( $loop->have_posts() ) :
+                                while ( $loop->have_posts() ) : $loop->the_post(); ?>
+                                    <?php if($i = 1) { ?>
+                                        <div class="col-sm-8 mt-md-3">
+                                            <div class="row">    
+                                    <?php } ?>
                                     <?php if($i < 3) { ?>
+                                        <div class="col-sm-6">
+                                            <?php if ( get_field( 'imagen') ) { ?>
+                                                    <img src="<?php the_field( 'imagen' ); ?>" class="img-fluid" data-toggle="modal"
+                                                        data-target="#exampleModal" data-whatever="@getbootstrap"/>
+                                                    <?php } ?>
+                                                    <!-- <img src="?php echo get_stylesheet_directory_uri(); ?>/images/about_2.jpg" class="img-fluid" data-toggle="modal"
+                                                        data-target="#exampleModal" data-whatever="@getbootstrap" /> -->
+                                                    <p class="text-center titulos-fotos-inicio" data-toggle="modal" data-target="#exampleModal"
+                                                        data-whatever="@getbootstrap">
+                                                        <a><?php the_field( 'titulo' ); ?></a>
+                                                    </p>
 
-                                    <div class="col-sm-6">
-                                        <?php if ( get_field( 'imagen') ) { ?>
-                                                <img src="<?php the_field( 'imagen' ); ?>" class="img-fluid" data-toggle="modal"
-                                                    data-target="#exampleModal" data-whatever="@getbootstrap"/>
-                                                <?php } ?>
-                                                <!-- <img src="?php echo get_stylesheet_directory_uri(); ?>/images/about_2.jpg" class="img-fluid" data-toggle="modal"
-                                                    data-target="#exampleModal" data-whatever="@getbootstrap" /> -->
-                                                <p class="text-center titulos-fotos-inicio" data-toggle="modal" data-target="#exampleModal"
-                                                    data-whatever="@getbootstrap">
-                                                    <a><?php the_field( 'titulo' ); ?></a>
-                                                </p>
-
-                                                <!-- MODAL -->
-                                                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-label
-                                                    ledby="exampleModalLabel " aria-hidden="true">
-                                                    <div class="modal-dialog" role="document">
-                                                        <div class="modal-content modales">
-                                                            <div class="modal-header">
-                                                                <h5 class="modal-title" id="exampleModalLabel" style="margin-left: auto;">
-                                                                    INSCRIBIRME
-                                                                </h5>
-                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                                    <span aria-hidden="true">&times;</span>
-                                                                </button>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                <form>
-                                                                    <div class="form-group">
-                                                                        <label for="recipient-name"
-                                                                            class="col-form-label ml-md-3 ml-2">Nombre</label>
-                                                                        <input type="text" class="form-control" id="recipient-name" />
-                                                                    </div>
-                                                                    <div class="form-group">
-                                                                        <label for="recipient-name"
-                                                                            class="col-form-label ml-md-3 ml-2">Apellido</label>
-                                                                        <input type="text" class="form-control" id="recipient-name" />
-                                                                    </div>
-                                                                    <div class="form-group">
-                                                                        <label for="recipient-name"
-                                                                            class="col-form-label ml-md-3 ml-2">E-Mail</label>
-                                                                        <input type="text" class="form-control" id="recipient-name" />
-                                                                    </div>
-                                                                    <div class="form-group">
-                                                                        <label for="recipient-name"
-                                                                            class="col-form-label ml-md-3 ml-2">Teléfono</label>
-                                                                        <input type="text" class="form-control" id="recipient-name" />
-                                                                    </div>
-                                                                    <div class="form-group text-center">
-                                                                        <label for="recipient-name" class="col-form-label">Es asesor de
-                                                                            Crea?</label>
-                                                                        <div>
-                                                                            <button class="btn" style="width: 50px">
-                                                                                SI
-                                                                            </button>
-                                                                            <button class="btn" style="width: 50px">
-                                                                                NO
-                                                                            </button>
+                                                    <!-- MODAL -->
+                                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-label
+                                                        ledby="exampleModalLabel " aria-hidden="true">
+                                                        <div class="modal-dialog" role="document">
+                                                            <div class="modal-content modales">
+                                                                <div class="modal-header">
+                                                                    <h5 class="modal-title" id="exampleModalLabel" style="margin-left: auto;">
+                                                                        INSCRIBIRME
+                                                                    </h5>
+                                                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                        <span aria-hidden="true">&times;</span>
+                                                                    </button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    <form>
+                                                                        <div class="form-group">
+                                                                            <label for="recipient-name"
+                                                                                class="col-form-label ml-md-3 ml-2">Nombre</label>
+                                                                            <input type="text" class="form-control" id="recipient-name" />
                                                                         </div>
-                                                                    </div>
-                                                                    <div class="form-group text-center">
-                                                                        <label for="recipient-name" class="col-form-label ">Es
-                                                                            Productor?</label>
-                                                                        <div>
-                                                                            <button class="btn" style="width: 50px">
-                                                                                SI
-                                                                            </button>
-                                                                            <button class="btn" style="width: 50px">
-                                                                                NO
-                                                                            </button>
+                                                                        <div class="form-group">
+                                                                            <label for="recipient-name"
+                                                                                class="col-form-label ml-md-3 ml-2">Apellido</label>
+                                                                            <input type="text" class="form-control" id="recipient-name" />
                                                                         </div>
-                                                                    </div>
-                                                                </form>
-                                                            </div>
-                                                            <div class="modal-footer">
-                                                                <button type="button" class="btn btn-cerrar" data-dismiss="modal">
-                                                                    Cerrar
-                                                                </button>
-                                                                <button type="button" class="btn btn-contacto">
-                                                                    Enviar
-                                                                </button>
+                                                                        <div class="form-group">
+                                                                            <label for="recipient-name"
+                                                                                class="col-form-label ml-md-3 ml-2">E-Mail</label>
+                                                                            <input type="text" class="form-control" id="recipient-name" />
+                                                                        </div>
+                                                                        <div class="form-group">
+                                                                            <label for="recipient-name"
+                                                                                class="col-form-label ml-md-3 ml-2">Teléfono</label>
+                                                                            <input type="text" class="form-control" id="recipient-name" />
+                                                                        </div>
+                                                                        <div class="form-group text-center">
+                                                                            <label for="recipient-name" class="col-form-label">Es asesor de
+                                                                                Crea?</label>
+                                                                            <div>
+                                                                                <button class="btn" style="width: 50px">
+                                                                                    SI
+                                                                                </button>
+                                                                                <button class="btn" style="width: 50px">
+                                                                                    NO
+                                                                                </button>
+                                                                            </div>
+                                                                        </div>
+                                                                        <div class="form-group text-center">
+                                                                            <label for="recipient-name" class="col-form-label ">Es
+                                                                                Productor?</label>
+                                                                            <div>
+                                                                                <button class="btn" style="width: 50px">
+                                                                                    SI
+                                                                                </button>
+                                                                                <button class="btn" style="width: 50px">
+                                                                                    NO
+                                                                                </button>
+                                                                            </div>
+                                                                        </div>
+                                                                    </form>
+                                                                </div>
+                                                                <div class="modal-footer">
+                                                                    <button type="button" class="btn btn-cerrar" data-dismiss="modal">
+                                                                        Cerrar
+                                                                    </button>
+                                                                    <button type="button" class="btn btn-contacto">
+                                                                        Enviar
+                                                                    </button>
+                                                                </div>
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                    
-                                    </div>
+                                        
+                                        </div>
                                     <?php } ?>
-                                    <?php if($i = 3) { ?>
-                                </div>
-                            </div>
-                            <!-- segunda columna: primer foto chiquita-->
-                            <div class="col-sm-4 mt-4 mt-md-3">
-                                <div class="row">
-                                <?php } ?>
+                                    <?php if($i = 2) {?>
+                                            </div>
+                                        </div>
+                                        <!-- segunda columna: primer foto chiquita-->
+                                        <div class="col-sm-4 mt-4 mt-md-3">
+                                            <div class="row">
+                                    <?php } ?>
                                     <?php if($i > 2) { ?>
                                         <div class="col-sm-6">
                                                 <?php if ( get_field( 'imagen') ) { ?>
@@ -216,10 +217,12 @@ Template Name: trabajos
                                             </div>
                                         </div>
                                     <?php } ?>                                
-                            <?php $i++; endwhile;?>
-                            </div>
-                <?php endif;?>
-                <?php wp_reset_postdata();?>
+                                            <?php $i++; endwhile;?>                                        
+                                <?php endif;?>
+                                <?php wp_reset_postdata();?>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
