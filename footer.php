957105c4a0eb8eced867cdcf2fee68f15c0d0cb7
Alex <alex@syloper.com>
2020-04-16 11:37:35 -0300

    footer

--- footer.php
+++ footer.php
@@ -4,10 +4,10 @@
         <!-- owl carrousel -->
         <div class="container-fluid">
             <div class="owl-carousel owl-theme">
-            <!-- ?php $args = array('post_type' => 'footer'); ?>
+            <?php $args = array('post_type' => 'footer'); ?>
                     <?php $loop = new WP_Query($args);
                     if ( $loop->have_posts() ) :
-                        while ( $loop->have_posts() ) : $loop->the_post(); ?>  -->
+                        while ( $loop->have_posts() ) : $loop->the_post(); ?>  
                 <div>
                 <?php if ( get_field( 'imagen') ) { ?>
                     <img src="<?php the_field( 'imagen' ); ?>" class="owl-images rounded-circle mx-auto"
