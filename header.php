d70b9bd77a35ee159a745f818562849975b8c918
Alex <alex@syloper.com>
2020-04-17 12:34:25 -0300

    nav

--- header.php
+++ header.php
@@ -79,13 +79,13 @@
 
     <div class="container">
         <div class="row ">
-            <div class="col-md-12">
+            <div class="col-md-12 ml-md-5">
                 <div class="row">
                     <div class="col-md-3">
                     </div>
                     <div class="col-md-3"></div>
                     <div class="col-md-3"></div>
-                    <div class="col-md-3 ml-5">
+                    <div class="col-md-3">
                         <?php
                         if ( function_exists('yoast_breadcrumb') ) {
                         yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
