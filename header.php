a302b331905b3f89c4a4e198ff351ab7b0d6880a
Alex <alex@syloper.com>
2020-04-17 12:13:20 -0300

    nav

--- header.php
+++ header.php
@@ -59,6 +59,7 @@
                         </li>
                         </ul>
                 </div>
+                <br>
                 <?php
                         if ( function_exists('yoast_breadcrumb') ) {
                         yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
