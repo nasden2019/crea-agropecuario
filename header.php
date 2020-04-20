85a9549010715c93f7ec33d418bd7e540736cf70
Alex <alex@syloper.com>
2020-04-20 13:29:32 -0300

    bred

--- header.php
+++ header.php
@@ -76,9 +76,9 @@
                 <div class="row">
                     <div class="col-md-3">
                     </div>
-                    <!-- <div class="col-md-3"></div> -->
                     <div class="col-md-3"></div>
-                    <div class="col-md-4">
+                    <div class="col-md-3"></div>
+                    <div class="col-md-3">
                         <?php
                         if ( function_exists('yoast_breadcrumb') ) {
                         yoast_breadcrumb( '<p id="breadcrumbs">','</p>' );
