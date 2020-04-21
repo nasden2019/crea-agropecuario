437ee8b7cdcf50313aa7933828cb07572a7ca573
Alex <alex@syloper.com>
2020-04-21 07:48:33 -0300

    ref

--- referencias.php
+++ referencias.php
@@ -32,14 +32,31 @@ Template Name: referencias
                         while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
           <div class="col-md-6">
-            <?php if ( get_field( 'imagen') ) { ?>
-            <img src="<?php the_field( 'imagen' ); ?>" />
-            <?php } ?>
+          <blockquote class="twitter-tweet">
+              <p lang="en" dir="ltr">Sunsets don&#39;t get much better than this one over <a
+                  href="https://twitter.com/GrandTetonNPS?ref_src=twsrc%5Etfw">@GrandTetonNPS</a>. <a
+                  href="https://twitter.com/hashtag/nature?src=hash&amp;ref_src=twsrc%5Etfw">#nature</a> <a
+                  href="https://twitter.com/hashtag/sunset?src=hash&amp;ref_src=twsrc%5Etfw">#sunset</a> <a
+                  href="http://t.co/YuKy2rcjyU">pic.twitter.com/YuKy2rcjyU</a></p>&mdash; US Department of the Interior
+              (@Interior) <a href="https://twitter.com/Interior/status/463440424141459456?ref_src=twsrc%5Etfw">May 5,
+                2014</a>
+            </blockquote>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <!-- ?php if ( get_field( 'imagen') ) { ?>
+            <img src="?php the_field( 'imagen' ); ?>" />
+            ?php } ?> -->
           </div>
           <div class="col-md-6">
-            <?php if ( get_field( 'imagen') ) { ?>
-            <img src="<?php the_field( 'imagen' ); ?>" />
-            <?php } ?>
+          <blockquote class="twitter-tweet">
+              <p lang="es" dir="ltr">El cirujano que operó a Kim Jong Un, ahora. <a
+                  href="https://t.co/YhMJOwVZyY">pic.twitter.com/YhMJOwVZyY</a></p>&mdash; Víctor Barreto (@victorbar67)
+              <a href="https://twitter.com/victorbar67/status/1252463571906199553?ref_src=twsrc%5Etfw">April 21,
+                2020</a>
+            </blockquote>
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <!-- ?php if ( get_field( 'imagen') ) { ?>
+            <img src="?php the_field( 'imagen' ); ?>" />
+            ?php } ?> -->
             <!-- <iframe frameborder=0 height=250 width=550 class="col-md-12"
                                 src="https://twitframe.com/show?url=https%3A%2F%2Ftwitter.com%2Fjack%2Fstatus%2F20"></iframe> -->
           </div>
