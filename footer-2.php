78e9634679217c0dc2437da5e75f7e1d971de102
Gino Caputo <ginopq@gmail.com>
2020-04-22 10:26:00 -0300

    registro

--- footer-2.php
+++ footer-2.php
@@ -103,11 +103,11 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script>
 
-$(document).ready(function () {
-            $('#sidebarCollapse').on('click', function () {
-                $('#sidebar').toggle();
-            });
-        });
+        // $(document).ready(function () {
+        //     $('#sidebarCollapse').on('click', function () {
+        //         $('#sidebar').toggle();
+        //     });
+        // });
         $(".owl-carousel").owlCarousel({
             loop: true,
             margin: 10,
