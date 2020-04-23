070d239d734536013f3d145d3a4bedf83289f00d
Alex <alex@syloper.com>
2020-04-23 07:43:24 -0300

    comments estilos

--- trabajaConNosotros.php
+++ trabajaConNosotros.php
@@ -87,42 +87,40 @@ Template Name: trabaja con nosotros
   </section>
 
 
-  <?php get_footer(); ?>
-
   <section class="section border-bottom">
       <div class="container">
         <!-- <h2 class="my-md-4">Dejanos un comentario:</h2> -->
         <div class="row">
-          <div class="col-md-9 my-5 order-2">
+          <div class="col-md-12 order-2">
             <form action="#" method="post">
               <div class="row">
-                <div class="col-md-4 form-group">
+                <div class="col-md-2 form-group">
                   <label for="nombre">Nombre</label>
                   <input type="text" id="nombre" class="form-control" />
                 </div>
-                <div class="col-md-3 form-group">
+                <div class="col-md-2 form-group">
                   <label for="phone">Teléfono</label>
                   <input type="text" id="tel" class="form-control" />
                 </div>
-                <div class="col-md-4 form-group">
+                <div class="col-md-2 form-group">
                   <label for="email">Email</label>
                   <input type="email" id="email" class="form-control" />
                 </div>
-                <div class="col-md-8 form-group">
+                <div class="col-md-6 form-group">
                   <label for="message">Comentarios</label>
                   <textarea
                     name="message"
                     id="mensaje"
                     class="form-control"
-                    cols="20"
-                    rows="4"
+                    cols="14"
+                    rows="3"
                   ></textarea>
                 </div>
-                <div class="col-md-4 mt-5 form-group">
+                <div class="col-md-4 form-group">
                   <input
                     type="submit"
                     value="Enviar"
-                    class="boton btn btn-contacto px-3 py-3"
+                    class="boton btn btn-contacto px-3"
                   />
                 </div>
               </div>
@@ -130,17 +128,17 @@ Template Name: trabaja con nosotros
               <!-- feedback usuario-->
               <span class="saludo"></span>
               <div class="row my-4">
-                <div class="col-md-4 form-group">
+                <div class="col-md-2 form-group">
                   <span class="span1"></span>
                 </div>
-                <div class="col-md-3 form-group">
+                <div class="col-md-2 form-group">
                   <span class="span2"></span>
                 </div>
-                <div class="col-md-4 form-group">
+                <div class="col-md-2 form-group">
                   <span class="span3"></span>
                 </div>
 
-                <div class="col-md-8 form-group">
+                <div class="col-md-6 form-group">
                   <span class="span4"></span>
                 </div>
               </div>
@@ -149,6 +147,10 @@ Template Name: trabaja con nosotros
         </div>
       </div>
     </section>
+
+    
+  <?php get_footer(); ?>
+
 
 <script>
   $(document).ready(function () {
