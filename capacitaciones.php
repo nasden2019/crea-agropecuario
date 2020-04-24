e9f615b57f3250e1c8445d29f9d55cf28264f454
Gino Caputo <ginopq@gmail.com>
2020-04-24 12:29:04 -0300

    form registro

--- capacitaciones.php
+++ capacitaciones.php
@@ -335,7 +335,8 @@ Template name: capacitacion
         <h2 class="my-md-5">Dejanos un comentario:</h2>
         <div class="row">
           <div class="col-md-12 order-2">
-          [contact-form-7 id="469" title="comentarios"]
+            <?php echo do_shortcode('[contact-form-7 id="469" title="comentarios"]'); ?>
+          
           </div>
         </div>
       </div>
