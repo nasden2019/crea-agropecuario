410d5d8ae48592ca82e3c5588a46a4527c729070
Gino Caputo <ginopq@gmail.com>
2020-04-24 12:05:09 -0300

    form registro

--- taxonomy-registro.php
+++ taxonomy-registro.php
@@ -12,9 +12,7 @@
                 $loop = new WP_Query($args);
                 if ( $loop->have_posts() ) :
                     while ( $loop->have_posts() ) : $loop->the_post();?>
-                    <input type="email" class="d-none" id="email-registro" value="<?php the_field( 'email' ); ?>">
-
-                    <?php 
+                    <input type="email" class="d-none" id="email-registro" value="<?php the_field( 'email' ); ?>">                    <?php 
                     endwhile;
                 endif;?>
                 <?php wp_reset_postdata();?>
