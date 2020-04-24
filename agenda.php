ccc3ce3fb4f15e46448918e9def3222f74fc2912
Alex <alex@syloper.com>
2020-04-24 09:31:46 -0300

    agenda

--- agenda.php
+++ agenda.php
@@ -10,24 +10,24 @@ Template name: agenda
 
    
     
-        <!-- <div class="my-5 text-center">
+        <!--  <div class="my-5 text-center"> 
             <h2>Calendario Crea</h2>
             <iframe
                 src="https://calendar.google.com/calendar/embed?src=gnp4oirdq97es7ibh252asalc0%40group.calendar.google.com&ctz=Europe%2FMadrid"
                 style="border: 0" width="80%" height="700" frameborder="0" scrolling="no"></iframe>
-        </div>
+        </div> -->
  
 
 
 
    
-    <div id="loader" class="show fullscreen">
+     <div id="loader" class="show fullscreen">
         <svg class="circular" width="48px" height="48px">
             <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
             <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
                 stroke="#f4b214" />
         </svg>
-    </div> -->
+    </div> 
 
    
 
