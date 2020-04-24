0cefe7c826abd598634099aab39cee3a4bd8b020
Gino Caputo <ginopq@gmail.com>
2020-04-24 13:14:54 -0300

    form registro

--- testeoHome.php
+++ testeoHome.php
@@ -209,57 +209,7 @@ Template Name: testeo home
         <h2 class="my-md-5">Dejanos un comentario:</h2>
         <div class="row">
           <div class="col-md-12 order-2">
-            <form action="#" method="post">
-              <div class="row">
-                <div class="col-md-2 form-group">
-                  <label for="nombre">Nombre</label>
-                  <input type="text" id="nombre" class="form-control" />
-                </div>
-                <div class="col-md-2 form-group">
-                  <label for="phone">Teléfono</label>
-                  <input type="text" id="tel" class="form-control" />
-                </div>
-                <div class="col-md-2 form-group">
-                  <label for="email">Email</label>
-                  <input type="email" id="email" class="form-control" />
-                </div>
-                <div class="col-md-6 form-group">
-                  <label for="message">Comentarios</label>
-                  <textarea
-                    name="message"
-                    id="mensaje"
-                    class="form-control"
-                    cols="14"
-                    rows="3"
-                  ></textarea>
-                </div>
-                <div class="col-md-4 form-group">
-                  <input
-                    type="submit"
-                    value="Enviar"
-                    class="boton btn btn-contacto px-3"
-                  />
-                </div>
-              </div>
-
-              <!-- feedback usuario-->
-              <span class="saludo"></span>
-              <div class="row my-4">
-                <div class="col-md-2 form-group">
-                  <span class="span1"></span>
-                </div>
-                <div class="col-md-2 form-group">
-                  <span class="span2"></span>
-                </div>
-                <div class="col-md-2 form-group">
-                  <span class="span3"></span>
-                </div>
-
-                <div class="col-md-6 form-group">
-                  <span class="span4"></span>
-                </div>
-              </div>
-            </form>
+           <?php echo do_shortcode('[contact-form-7 id="469" title="comentarios"]'); ?>
           </div>
         </div>
       </div>
