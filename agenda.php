4af644e1894e72a09722beda2d5261fb3e4f5187
Alex <alex@syloper.com>
2020-04-27 11:46:35 -0300

    agenda

--- agenda.php
+++ agenda.php
@@ -93,7 +93,15 @@ Template name: agenda
     </div>
  
 
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/main.js"></script>
 
+    <script src="js/main.js"></script>
 
    
    <!-- para que el modal funcione en .php -->
