d1ac97f7c2067061e664ae49c879a2b85808b43a
Gino Caputo <ginopq@gmail.com>
2020-04-27 11:10:20 -0300

    cambios nuevo comentarios

--- footer-comentarios.php
+++ footer-comentarios.php
@@ -12,7 +12,7 @@
                     endwhile;
                 endif;?>
                 <?php wp_reset_postdata();?>
-            <?php echo do_shortcode('[contact-form-7 id="469" title="comentarios" html_class="formulario"]'); ?>
+            <?php echo do_shortcode('[contact-form-7 id="469" title="comentarios" html_class="formulario" class="formulario"]'); ?>
           </div>
         </div>
       </div>
