48a7a57c2664636a2c2dfe516843d56ebd5dd2a7
Gino Caputo <ginopq@gmail.com>
2020-04-28 17:16:58 -0300

    cambios

--- footer-comentarios.php
+++ footer-comentarios.php
@@ -1,8 +1,8 @@
 
       <div class="container-fluid">
       <div class="row justify-content-center" style="background: #7c7c7c45;">
-        <div class="col-4 text-center">        
-        <button class="comentar my-md-3 btn btn-contacto"  style="width: 75%!important;">Dejar un comentario</button>
+        <div class="col-8 col-md-4 text-center">        
+        <button class="comentar my-4 my-md-3 btn btn-contacto"  style="width: 75%!important;">Dejar un comentario</button>
         </div>
       </div>
 
