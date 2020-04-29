fe25203bd261f2695e028f8a37350347bb91f3da
Gino Caputo <ginopq@gmail.com>
2020-04-29 16:48:58 -0300

    cambios: sidebar proyectos en mobile

--- agenda.php
+++ agenda.php
@@ -70,7 +70,7 @@ Template name: agenda
                                     </button>
                                 </div>
                                 <div class="modal-body">
-                                <!-- <?php echo do_shortcode('[contact-form-7 id="484" title="agenda"]'); ?> -->
+                                    <?php echo do_shortcode('[contact-form-7 id="484" title="agenda"]'); ?>
                                 </div>
                                 <!-- <div class="modal-footer">
                                 <button type="button" class="btn btn-cerrar" data-dismiss="modal">
