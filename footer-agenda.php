c859112a4640332634317d914af6425c3f437181
Gino Caputo <ginopq@gmail.com>
2020-04-30 10:05:33 -0300

    cambios en slug trabaja con nosotros mobile

--- footer-agenda.php
+++ footer-agenda.php
@@ -1,12 +1,12 @@
-    <script src="js/jquery-3.2.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.waypoints.min.js"></script>
-    <script src="js/jquery.fancybox.min.js"></script>
-    <script src="js/main.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/popper.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/bootstrap.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/owl.carousel.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.waypoints.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/jquery.fancybox.min.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/main.js"></script>
 
-    <script src="js/main.js"></script>
+    <script src="<?php echo get_stylesheet_directory_uri(); ?>/js/main.js"></script>
 
     
     <script>
@@ -73,6 +73,7 @@
             SPU.show( 143 ); 
             SPU.hide( 120 );
             SPU.track( 143, false ); 
+            console.log("prueba");
         });
     </script>
     <script>        
